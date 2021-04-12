5c807cea490be1527d9ae2dc40c3b06595a448a6
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-04-12 20:35:25 -0300

    Initial commit

---
+++ IdentityTest/IdentityTest/Data/IdentityTestDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityTest.Data
+{
+    public class IdentityTestDbContext: IdentityDbContext
+    {
+        public IdentityTestDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ IdentityTest/IdentityTest/Startup.cs
@@ -0,0 +1,71 @@
+using IdentityTest.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //todo: diff
+            services.AddDbContext<IdentityTestDbContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("IdentityTestDbContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)                
+                .AddEntityFrameworkStores<IdentityTestDbContext>();
+
+            services.AddControllersWithViews();
+
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage(); todo: diff
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseAuthentication();            
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
